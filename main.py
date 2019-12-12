5804cce5e069bb3295cc44bf0b2cfc7cd2b20a6f
PlugNCS <michael.nass@free.fr>
2019-12-12 17:02:03 +0100

    UPD

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
+field = []
+
+def initField(x, y):
+    for i in range(0, x):
+        field.append([])
+        for s in range(0, y):
+            field[i].append(-1)
+            
+def printField():
+    for line in field:
+        for s in range(0, len(line)):
+            if line[s] == -1:
+                print("🟩", "", end='')
+            elif line[s] == 0:
+                print("⚫️", "", end='')
+            elif line[s] == 1:
+                print("⚪️", "", end='')
+        print("")
+
+def place(x, y, player):
+    field[x][y] = player
+
+initField(8, 8)
+place(3, 2, 0)
+place(2, 5, 1)
+printField()
+
+
+
+
+
 
